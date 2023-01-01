dc45c9686011cd6a2f0fd8c07343030fb59c8c86
Gustavo Nep <gustavo._12@hotmail.com>
2023-01-01 18:42:41 -0300

    button

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -26,10 +26,10 @@ const Business = () => {
   return (
     <section id="features" className={layout.section}>
       <div className={layout.sectionInfo}>
-      <h2 className={styles.heading2}>Você faz os negócios, <br className="sm:block hidden"></br> e nós vamos cuidar do dinheiro.</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Com o cartão de crédito certo, você pode melhorar sua vida financeira acumulando crédito, 
-      ganhando recompensas e economizando dinheiro. Mas com centenas de cartões de crédito no mercado.</p>
-      <Button styles="mt-10"/>
+        <h2 className={styles.heading2}>Você faz os negócios, <br className="sm:block hidden"></br> e nós vamos cuidar do dinheiro.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Com o cartão de crédito certo, você pode melhorar sua vida financeira acumulando crédito, 
+        ganhando recompensas e economizando dinheiro. Mas com centenas de cartões de crédito no mercado.</p>
+        <Button styles="mt-10"/>
       </div>
       
       <div className={`${layout.sectionImg} flex-col`}>
